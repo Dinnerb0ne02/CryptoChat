f42fae9c65477a8cca15e98aec686298b52bf688
Dinnerb0ne <tomma_2022@outlook.com>
2025-05-14 20:07:19 +0800

    fix: Update copyright and license information

--- chat.py
+++ chat.py
@@ -1,7 +1,22 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) CryptoChat Dinnerb0ne<tomma_2022@outlook.com>
-# date: 2023-10-01
-# version: 1.1.0
+# Copyright 2025(C) CryptoChat Dinnerb0ne<tomma_2022@outlook.com>
+
+#    Copyright 2025 [Dinnberb0ne & T0ast101]
+
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+
+#        http://www.apache.org/licenses/LICENSE-2.0
+
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+# date: 2025-05-14
+# version: 1.1.2
 # description: A simple chat application with encryption and room features.
 # LICENSE: Apache-2.0
 
